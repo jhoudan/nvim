574633cd1498616c2c41a502c64bb8d7fd2622be
jhoudan <jerom.houdan@gmail.com>
2024-05-18 16:29:33 +0200

    refactor: flatten lua plugins folder

--- init.lua
+++ init.lua
@@ -13,7 +13,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ import = "custom/plugins" }, {
+require("lazy").setup({ import = "plugins" }, {
   change_detection = {
     notify = false,
   },
