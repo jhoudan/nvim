ccf4fe94648a26998df83d6c7adb30e260498c29
jhoudan <jerom.houdan@gmail.com>
2024-05-18 22:35:19 +0200

    feat: add a bunch of colorschemes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -17,4 +17,38 @@ return {
       vim.cmd.colorscheme("tokyodark")
     end,
   },
+  "rktjmp/lush.nvim",
+  "xero/miasma.nvim",
+  "tjdevries/colorbuddy.nvim",
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "catppuccin/nvim", name = "catppuccin" },
+  {
+    "sainnhe/gruvbox-material",
+    config = function()
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_foreground = "mix"
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      styles = {
+        comments = { italic = false },
+        keywords = { italic = false },
+        identifiers = { italic = false },
+      },
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      require("kanagawa").setup({
+        commentStyle = { italic = false },
+        keywordStyle = { italic = false },
+      })
+    end,
+  },
 }
