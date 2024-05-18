0ee280f746dab92c4e90a0e6608e083ce6dcf996
jhoudan <jerom.houdan@gmail.com>
2024-05-18 20:50:58 +0200

    feat: setup lsp configuration

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,85 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "folke/neodev.nvim",
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      { "j-hui/fidget.nvim", opts = {} },
+    },
+    config = function()
+      require("neodev").setup()
+
+      local capabilities = nil
+      if pcall(require, "cmp_nvim_lsp") then
+        capabilities = require("cmp_nvim_lsp").default_capabilities()
+      end
+
+      local lspconfig = require("lspconfig")
+
+      -- check :help lspconfig-server-configuration for more details
+      local servers = {
+        lua_ls = {},
+        bashls = {},
+        tsserver = {},
+        cssls = {},
+        ruby_lsp = {},
+        -- elixirls = {
+        --   cmd = { "/path/to/elixir-ls/language_server.sh" };
+        -- },
+      }
+
+      local servers_to_install = vim.tbl_filter(function(key)
+        local t = servers[key]
+        if type(t) == "table" then
+          return not t.manual_install
+        else
+          return t
+        end
+      end, vim.tbl_keys(servers))
+
+      require("mason").setup()
+      local ensure_installed = {
+        "stylua",
+        "lua_ls",
+      }
+
+      vim.list_extend(ensure_installed, servers_to_install)
+      require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+      for name, config in pairs(servers) do
+        config = vim.tbl_deep_extend("force", {}, {
+          capabilities = capabilities,
+        }, config)
+
+        lspconfig[name].setup(config)
+      end
+
+      -- Disable semantic tokens by default to avoid syntax highlighting issues w/ treesitter
+      local enable_semantic_tokens = {}
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local bufnr = args.buf
+          local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
+
+          vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = 0 })
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
+          vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+
+          vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = 0 })
+          vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0 })
+
+          local filetype = vim.bo[bufnr].filetype
+          if not enable_semantic_tokens[filetype] then
+            client.server_capabilities.semanticTokensProvider = nil
+          end
+        end,
+      })
+    end,
+  },
+}
