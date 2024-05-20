61521e1dc19b80eacbaca9c500300b6cd987859d
jhoudan <jerom.houdan@gmail.com>
2024-05-20 19:19:29 +0200

    feat(keymaps): add diagnostics navigation bindings

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -9,3 +9,7 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Keybinds to navigate between diagnostics
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
